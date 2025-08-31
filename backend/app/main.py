0603f37c35a1d7fde4204ddec4f0d9d2d1a85db4
Shorya <Shorya34@gmail.com>
2025-09-01 00:03:45 +0530

    disabled cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,7 +19,7 @@ def create_app() -> FastAPI:
     # Enable CORS
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=settings.CORS_ALLOWED_ORIGINS,
+        allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
